384bbfcb2f221d2444bd62c81940160ddc92b1d6
Thomas Nance <thomas.nance@outlook.com>
2024-09-20 16:21:40 -0400

    Added parallelism to downloading images

--- methods/APISearchService.py
+++ methods/APISearchService.py
@@ -1,8 +1,11 @@
 import bpy
 import os
+from concurrent.futures import ThreadPoolExecutor
 from ..methods import helpermethods
 from ..methods import database
 from ..methods import webcalls
+
+searchResultImageCount = 0
 
 @helpermethods.background
 def GetAPISearchResultsByName(searchText, context):
@@ -11,13 +14,12 @@ def GetAPISearchResultsByName(searchText, context):
         context.window_manager.d2ci.SearchResultsText = "Searching Database..."
         queryResults = database.QueryManifestByName('DestinyInventoryItemDefinition', searchText)
         numOfFoundResults = str(len(queryResults.keys()))
-
+        global searchResultImageCount
+        searchResultImageCount = 0
         os.makedirs(helpermethods.GetProjectTempImagePath(), mode=0o777, exist_ok=True)
-        imgCount = 0
-        for key, value in queryResults.items():
-            imgCount += 1
-            context.window_manager.d2ci.SearchResultsText = f"Found {numOfFoundResults} result{'s' if numOfFoundResults == '1' else ''}. Downloading image {str(imgCount)}/{numOfFoundResults}..."
-            GetAndSaveImage(value.get('displayProperties').get('icon'), os.path.join(helpermethods.GetProjectTempImagePath(), str(key)+ ".jpg"))
+        with ThreadPoolExecutor(max_workers=6) as executor:
+            for key, value in queryResults.items():
+                executor.submit(GetAndSaveImage, value.get('displayProperties').get('icon'), os.path.join(helpermethods.GetProjectTempImagePath(), str(key)+ ".jpg"), numOfFoundResults, context)
 
         bpy.types.WindowManager.d2ci_search_results_manager.SetQueryResultsFromSearch(queryResults)
         context.window_manager.d2ci.IsSearchingAPI = False
@@ -28,10 +30,14 @@ def GetAPISearchResultsByName(searchText, context):
         context.window_manager.d2ci.IsSearchingAPI = False
         context.window_manager.d2ci.SearchResultsText = "Failed to query the database."
 
-def GetAndSaveImage(url, downloadName):
+def GetAndSaveImage(url, downloadName, numOfFoundResults, context):
     path = "https://bungie.net" + url
     imageData = webcalls.Get(path)
     if imageData.status_code == 200:
         with open(downloadName, 'wb') as f:
             for chunk in imageData:
                 f.write(chunk)
+    
+    global searchResultImageCount
+    searchResultImageCount += 1
+    context.window_manager.d2ci.SearchResultsText = f"Found {numOfFoundResults} result{'s' if numOfFoundResults == '1' else ''}. Downloaded image {str(searchResultImageCount)}/{numOfFoundResults}..."
